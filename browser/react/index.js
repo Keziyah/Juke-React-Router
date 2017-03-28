8012f9f28e8004b85bf7a2c4508e9e9ae31b1e36
Keziyah <keziyahlewis@gmail.com>
2017-03-28 17:02:19 -0400

    initial commit

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Router, Route, hashHistory} from 'react-router';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer} />
+  </Router>,
   document.getElementById('app')
 );
